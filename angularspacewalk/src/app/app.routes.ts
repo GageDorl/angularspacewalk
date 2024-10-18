a84b59174c2eba93e732a6ba981c4ddaf122cb62
GageDorl <gagedorlando@gmail.com>
2024-10-18 09:45:27 -0600

    Added routes'

--- angularspacewalk/src/app/app.routes.ts
+++ angularspacewalk/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../Components/home/home.component';
 import { ContactComponent } from '../Components/contact/contact.component';
+import { AboutComponent } from '../Components/about/about.component';
+import { ShopComponent } from '../Components/shop/shop.component';
 
 export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'shop', component: ShopComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'home', component: HomeComponent},
     { path: '', redirectTo:'home', pathMatch: 'full' }
